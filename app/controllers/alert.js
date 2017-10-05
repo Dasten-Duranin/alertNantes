b8335c14ecdb7a9329ed77f854f1d5712944c707
Thomas Berthé <berthe.thomas01@gmail.com>
2017-10-05 16:28:22 +0200

    indent update

--- app/controllers/alert.js
+++ app/controllers/alert.js
@@ -1,14 +1,14 @@
 var express = require('express'),
-	router  = express.Router(),
-	Alert   = require('../models/Alert'),
-	global  = require('../global');
+        router = express.Router(),
+        Alert = require('../models/Alert'),
+        global = require('../global');
 
-router.get('/', function(req, res, next){
+router.get('/', function (req, res, next) {
 
-	Alert.getAlert(function(warning){
-		res.setHeader('Content-Type', 'application/json');
-		res.send(JSON.stringify(warning));
-	})
+    Alert.getAlert(function (warning) {
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify(warning));
+    })
 });
 
 module.exports = router;
